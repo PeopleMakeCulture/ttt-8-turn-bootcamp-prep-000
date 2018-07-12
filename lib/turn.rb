634067c117ea2255130beeeb76914c174a1a1462
Jing Cao <jingcao.yale@gmail.com>
2018-07-12 17:14:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,12 +7,11 @@ def turn(board)
 
   if valid_move?(board,index) == false
   # asks for input again
-    while valid_move?(board,index) == false
     puts "Not a valid move. Try again."
     puts "Please enter 1-9:"
     user_number = gets.strip
     index = input_to_index(user_number)
-  
+
   else
   #makes valid moves
     move(board, index, char="X")
