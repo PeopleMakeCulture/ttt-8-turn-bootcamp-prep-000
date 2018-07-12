343322b098a26d3cd060f2b22b4c190d5000e151
Jing Cao <jingcao.yale@gmail.com>
2018-07-12 16:58:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,20 @@
 def turn(board)
   puts "Please enter 1-9:"
-  
+  user_number = gets.strip
+  # Call input_to_index
+  index = input_to_index(user_number)
+
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# gets location of play
+def input_to_index (user_number)
+  index = user_number.to_i - 1
 end
